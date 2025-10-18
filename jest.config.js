1b9c6165a0b59474a43d59c2ab8cf72a02255eff
Roody <roodyalexis36@gmail.com>
2025-10-18 22:57:10 +0200

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
-const { createDefaultPreset } = require("ts-jest");
+const {createDefaultPreset} = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+	testEnvironment: "node",
+	transform: {
+		...tsJestTransformCfg,
+	},
+	// Ignore compiled files in dist to prevent Jest from trying to parse ESM imports
+	testPathIgnorePatterns: ["<rootDir>/dist/"],
 };
